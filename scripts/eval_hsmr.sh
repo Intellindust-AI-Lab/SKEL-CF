fb92fa534b2972ab0c791223dcdaa4a5c7374756
Dali936 <li944104439@gmail.com>
2025-11-27 12:42:05 +0800

    1127-test-eval

--- scripts/eval_hsmr.sh
+++ scripts/eval_hsmr.sh
@@ -1,11 +1,11 @@
 # export NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1
 PYTHONPATH=. torchrun   run_hsmr_test.py \
                         trainer.test_batch_size=64 \
-                        exp_name=1127-test\
                         general.num_workers=12 \
                         hub.skel_head.transformer_decoder.context_dim=1280 \
                         policy.img_patch_size=256 \
-                        trainer.ckpt_path=/public_hw/home/cit_yuzeng/skelvit/data_outputs/exp/1105_aux_loss_base_0.1/checkpoints/best.pth
+                        trainer.ckpt_path=''\
+                        exp_name=''
 
 
 
