aa484ca807e97bf2dc95a1f45313ed949e46ce43
Dalii <li944104439@gmail.com>
2025-11-27 15:49:02 +0800

    Add files via upload

---
+++ vis/run_demo.sh
@@ -0,0 +1,16 @@
+CUDA_VISIBLE_DEVICES=8 PYTHONPATH=. python vis/run_demo.py \
+                  trainer.ckpt_path='' \
+                  misc.image_folder=''\
+                  misc.output_folder=''
+
+
+
+
+
+
+
+
+
+
+
+    
